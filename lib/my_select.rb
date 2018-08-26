a361fca168c375469e19a4394738c87ec21a7853
katy@karstaway.com <katy@karstaway.com>
2018-08-26 07:18:31 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,16 @@
 def my_select(collection)
- # your code here!
+ if block_given?
+  i = 0
+
+ while i < collection.length
+	 yield(collection[i])
+i = i + 1
 end
+	if collection[i].even?
+	  array.push
+collection.
+	 else
+puts nil
+end
+end
+
